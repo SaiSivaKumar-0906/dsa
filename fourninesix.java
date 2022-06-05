206d4df22ad11e11f31dd47521cb5d50719bee9f
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-06-05 22:06:05 +0530

    Add files via upload

---
+++ fourninesix.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class fourninesix {
+     public static void main(String[] args){
+       int[] one = {2, 4};
+       int[] two = {1,2,3,4};
+       int[] three = nextGreaterElement(one, two);
+       System.out.println(Arrays.toString(three));
+     }
+    static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] nge = new int[nums1.length];
+    Stack<Integer> st = new Stack<>();
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < nums2.length; i++) {
+        
+        while (!st.isEmpty() && nums2[i] > st.peek()) {
+            map.put(st.pop(), nums2[i]);
+        }
+
+        st.push(nums2[i]);
+    }
+
+    for (int i = 0; i < nums1.length; i++) {
+        nge[i] = map.getOrDefault(nums1[i], -1);
+    }
+
+    return nge;
+}
+    }
