8895686942374bc2d606994e75fb7175e3cc6607
siva kumar <99823143+SaiSivaKumar-0906@users.noreply.github.com>
2022-07-16 23:38:54 +0530

    Add files via upload

--- onesevenonesix.java
+++ onesevenonesix.java
@@ -1,22 +1,24 @@
 public class onesevenonesix {
      public static void main(String[] args){
-       int n = 4;
+       int n = 7;
        int three = totalMoney(n);
        System.out.println(three);
      }
      static int totalMoney(int n){
-        int weekday = 1,count=1;
-        int sum = 0;
-        for(int i=1;i<=n;i++)
-        {
-            sum+=count;
-            count++;
-            if(i%7==0)
-            {
-                weekday++;
-                count=weekday;
-            }
-        }
-        return sum;
+      int weekday = 1,count=1;
+      int sum = 0;
+      int i=1;
+      while(i<=n)
+      {
+          sum+=count;
+          count++;
+          if(i%7==0)
+          {
+              weekday++;
+              count=weekday;
+          }
+          i++;
+      }
+      return sum;
      }
 }
